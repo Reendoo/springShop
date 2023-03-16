fc8678044d4b5e256f906b6a3ba7e5924a170a07
Rendo <rendekml@peterrendek.sk>
2023-03-16 16:58:57 +0100

    firstCommit

---
+++ src/main/java/sk/peterrendek/learn2code/springshop/ShopSpringApplication.java
@@ -0,0 +1,13 @@
+package sk.peterrendek.learn2code.springshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/peterrendek/learn2code/springshop/ShopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.peterrendek.learn2code.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
